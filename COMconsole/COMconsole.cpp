be5c209847cff933fa0dab3cd990eab1a77e1ee8
dmxjMao <dmxjMao@DESKTOP-04FEPQT>
2017-09-24 22:46:10 +0800

    Add project files.

---
+++ COMconsole/COMconsole.cpp
@@ -0,0 +1,18 @@
+// COMconsole.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+//#include <iostream>
+//#include <objbase.h>
+//
+//using namespace std;
+
+//int main()
+//{
+//	cout << "hello,world!";
+//
+//	
+//
+//    return 0;
+//}
+
